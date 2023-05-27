35674428e097a94454a47ca3c0b552e3b8d43664
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-05-27 06:11:48 +0000

    [fix]ログイン処理

--- app/controllers/worker/sessions_controller.rb
+++ app/controllers/worker/sessions_controller.rb
@@ -11,15 +11,16 @@ class Worker::SessionsController < Devise::SessionsController
 def worker_state
   ## 【処理内容1】 入力されたemailからアカウントを1件取得
   @worker = Worker.find_by(email: params[:worker][:email])
+
   ## アカウントを取得できなかった場合、このメソッドを終了する
   return if !@worker
   ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
   if @worker.is_deleted
     return
   end
-  if @worker.valid_password?(params[:worker][:password])
-    ## 【処理内容3】
-    redirect_to root_path
-  end
+  # if @worker.valid_password?(params[:worker][:password])
+  #   ## 【処理内容3】
+  #   redirect_to root_path
+  # end
 end
 end
